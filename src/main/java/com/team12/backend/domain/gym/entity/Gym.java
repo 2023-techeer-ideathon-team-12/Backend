0eeaf98c2acbdb4a69cfe531189810abbb6690d8
hangyeol <snake011128@naver.com>
2023-05-27 02:24:27 +0900

    Gym Entity 추가

---
+++ src/main/java/com/team12/backend/domain/gym/entity/Gym.java
@@ -0,0 +1,90 @@
+package com.team12.backend.domain.gym.entity;
+
+import com.sun.istack.NotNull;
+import com.team12.backend.global.common.BaseEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "gyms")
+public class Gym extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "gym_id")
+    private Long id;
+
+    @Column(name = "gym_name")
+    @NotNull
+    private String name;
+
+    @Column
+    @NotNull
+    @ColumnDefault("0")
+    private String address;
+
+    @Column(name = "currentUser_number")
+    @NotNull
+    private Integer currentUser;
+
+    @Column
+    @NotNull
+    private Integer area;
+
+    @Column
+    @NotNull
+    private String machine;
+
+    @Column
+    @NotNull
+    private Double starAvg;
+
+    @Column
+    @NotNull
+    private String thumbnailUrl;
+
+    @Column(name = "gym_latitude")
+    @NotNull
+    private Double latitude;
+
+    @Column(name = "gym_longitude")
+    @NotNull
+    private Double longitude;
+
+    @Builder
+    public Gym(
+        String name,
+        String address,
+        Integer currentUser,
+        Integer area,
+        String machine,
+        Double starAvg,
+        String thumbnailUrl,
+        Double latitude,
+        Double longitude
+    ) {
+        this.name = name;
+        this.address = address;
+        this.currentUser = currentUser;
+        this.area = area;
+        this.machine = machine;
+        this. starAvg = starAvg;
+        this.thumbnailUrl = thumbnailUrl;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public void increaseCurrentUser() {
+        this.currentUser += 1;
+    }
+}
